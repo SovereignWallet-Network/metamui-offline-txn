72bd303f3a1e9bcb7eba01c801d92a3d281ae7f1
fennsqubix <92010960+fennsqubix@users.noreply.github.com>
2022-05-05 18:42:41 +0530

    Pushing lib to repo

---
+++ lib/index.d.ts
@@ -0,0 +1,20 @@
+import { GetRegistryOptsCore, TypeRegistry } from '@substrate/txwrapper-core';
+import * as metamuiMethods from './methods';
+import { KNOWN_CHAIN_PROPERTIES } from './utils/util';
+export * from './utils/util';
+export declare const methods: {
+    balances: typeof metamuiMethods.balances;
+};
+export * from '@substrate/txwrapper-core';
+/**
+ * Options for txwrapper-polkadot's `getRegistry` function.
+ */
+export interface GetRegistryOpts extends GetRegistryOptsCore {
+    specName: keyof typeof KNOWN_CHAIN_PROPERTIES;
+}
+/**
+ * Get a type registry for networks that txwrapper-polkadot supports.
+ *
+ * @param GetRegistryOptions specName, chainName, specVersion, and metadataRpc of the current runtime
+ */
+export declare function getRegistry({ specName, chainName, specVersion, metadataRpc, properties, }: GetRegistryOpts): TypeRegistry;
